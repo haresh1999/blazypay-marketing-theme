4a8529d15835ad783a4ef10860d9ae7e7b2c3a7c
varsh09m <varsh09m@gmail.com>
2025-05-20 16:59:53 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,11 +26,40 @@ document.querySelectorAll('.custom-faq .accordion-button').forEach(button => {
 
 
 // learning-page slider
-var swiper = new Swiper(".mySwiper4", {
+let mySwiper = new Swiper(".mySwiper4", {
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
+// 20-05-25 start
+
+let sellerSwiper = new Swiper(".seller-swiper", {
+  spaceBetween: 30,
+  slidesPerView: 4,           
+  slidesPerGroup: 1,        
+  loop: true,               
+  speed: 800,                 
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+let reviewSwiper = new Swiper(".review-swiper", {
+  spaceBetween: 24,
+  slidesPerView:4,           
+  slidesPerGroup: 1,                     
+  speed: 800,   
+  loop: true,   
+   navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },  
+});
+
+// 20-05-25 end
+
 
