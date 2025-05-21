3605eb04a81e4571c0b44960dc018a05f2ee595d
varsh09m <varsh09m@gmail.com>
2025-05-21 16:45:02 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,26 @@
 // plan-page banner slider
 var swiper = new Swiper(".custom-plan-slider", {
   loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-  
+
 // faq-section
 document.querySelectorAll('.custom-faq .accordion-button').forEach(button => {
   if (!button.classList.contains('collapsed')) {
-      button.closest('.accordion-item').classList.add('active');
+    button.closest('.accordion-item').classList.add('active');
   }
 
-  button.addEventListener('click', function() {
-      document.querySelectorAll('.custom-faq .accordion-item').forEach(item => {
-          item.classList.remove('active');
-      });
+  button.addEventListener('click', function () {
+    document.querySelectorAll('.custom-faq .accordion-item').forEach(item => {
+      item.classList.remove('active');
+    });
 
-      if (!this.classList.contains('collapsed')) {
-          this.closest('.accordion-item').classList.add('active');
-      }
+    if (!this.classList.contains('collapsed')) {
+      this.closest('.accordion-item').classList.add('active');
+    }
   });
 });
 
@@ -36,28 +36,62 @@ let mySwiper = new Swiper(".mySwiper4", {
 
 // 20-05-25 start
 
+// seller-swiper
+
 let sellerSwiper = new Swiper(".seller-swiper", {
   spaceBetween: 30,
-  slidesPerView: 4,           
-  slidesPerGroup: 1,        
-  loop: true,               
-  speed: 800,                 
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+  loop: true,
+  speed: 800,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    767: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+  },
 });
+
+
+// review slider
 
 let reviewSwiper = new Swiper(".review-swiper", {
   spaceBetween: 24,
-  slidesPerView:4,           
-  slidesPerGroup: 1,                     
-  speed: 800,   
-  loop: true,   
-   navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },  
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+  speed: 800,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
 });
 
 // 20-05-25 end
